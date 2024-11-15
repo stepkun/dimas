c26372db79b10a77a6b3c3eef7846f46af8a44c0
stepkun <stephan.kunz@kabelbw.de>
2024-11-15 17:24:17 +0100

    fix querier example

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -15,6 +15,16 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ### Removed
 
+## [0.4.2] - 2024-??-??
+
+### Added
+
+### Changed
+
+### Fixed
+
+### Removed
+
 ## [0.4.1] - 2024-11-13
 
 ### Fixed
--- dimas/examples/querier/main.rs
+++ dimas/examples/querier/main.rs
@@ -4,18 +4,21 @@
 use dimas::prelude::*;
 
 #[derive(Debug)]
-struct AgentProps {}
+struct AgentProps {
+	counter: u128,
+}
 
-async fn query_callback(_ctx: Context<AgentProps>, response: QueryableMsg) -> Result<()> {
+async fn query_callback(ctx: Context<AgentProps>, response: QueryableMsg) -> Result<()> {
 	let message: u128 = response.decode()?;
-	println!("Response 1 is '{message}'");
+	println!("Response [{}] is '{message}'", ctx.read()?.counter);
+	ctx.write()?.counter += 1;
 	Ok(())
 }
 
 #[dimas::main]
 async fn main() -> Result<()> {
 	// create & initialize agents properties
-	let properties = AgentProps {};
+	let properties = AgentProps { counter: 0 };
 
 	// create an agent with the properties and the prefix 'examples'
 	let agent = Agent::new(properties)
@@ -32,17 +35,16 @@ async fn main() -> Result<()> {
 
 	// timer for regular querying
 	let interval = Duration::from_secs(1);
-	let mut counter1 = 0i128;
 	agent
 		.timer()
 		.name("timer")
 		.interval(interval)
 		.callback(move |ctx| -> Result<()> {
-			println!("Querying [{counter1}]");
-			let message = Message::encode(&counter1);
+			let counter = ctx.read()?.counter;
+			println!("Querying [{counter}]");
+			let message = Message::encode(&counter);
 			// querying with stored query
 			ctx.get("query", Some(message), None)?;
-			counter1 += 1;
 			Ok(())
 		})
 		.add()?;
